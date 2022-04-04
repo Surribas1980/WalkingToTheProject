37ad85752c0109da070f7e737ff21293bbe5b3d4
Surribas1980 <issupl@gmail.com>
2022-04-04 23:09:33 +0200

    1 file modified, 1 added : Introducing NavBar

---
+++ src/componentes/elNavBar/NavBar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function NavBar() {
+  return (
+    <nav>
+      <ul className="nav-items">
+        <li className="nav-item">Section 1</li>
+        <li className="nav-item">Section 2</li>
+        <li className="nav-item">Section 3</li>
+        <li className="nav-item">Section 4</li>
+        <li className="nav-item">Section 5</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default NavBar;
