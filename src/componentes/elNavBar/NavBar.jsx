54f9c1830f4f1988ab6d6c2762b4950fbcfe7dcd
Surribas1980 <issupl@gmail.com>
2022-04-05 12:15:02 +0200

    src/componentes/elNavBar/NavBar.jsx - NEW Component - For scroll in Main

--- src/componentes/elNavBar/NavBar.jsx
+++ src/componentes/elNavBar/NavBar.jsx
@@ -1,14 +1,47 @@
 import React from 'react';
+import {Link} from 'react-scroll';
 
 function NavBar() {
   return (
     <nav>
-      <ul className="nav-items">
-        <li className="nav-item">Section 1</li>
-        <li className="nav-item">Section 2</li>
-        <li className="nav-item">Section 3</li>
-        <li className="nav-item">Section 4</li>
-        <li className="nav-item">Section 5</li>
+      <ul className="nav-items divFlex">
+        <li className="nav-item">
+        <Link activeClass="active"
+                    
+                    to="seccion1"
+                    spy={true}
+                   
+                    smooth={true}
+                    duration={500}
+                    offset={-50}>
+        Section 1
+        </Link>  
+        </li>
+        <li className="nav-item">
+          <Link activeClass="active"
+                    
+                    to="seccion2"
+                    spy={true}
+                    
+                    smooth={true}
+                    duration={500}
+                    offset={-50}>
+          Section 2
+          </Link> 
+        </li>
+        <li className="nav-item">
+          <Link activeClass="active"
+                   
+                    to="seccion3"
+                    spy={true}
+                   
+                    smooth={true}
+                    duration={500}
+                    offset={-50}>
+          Section 3
+          </Link>
+          </li>
+        
       </ul>
     </nav>
   );
