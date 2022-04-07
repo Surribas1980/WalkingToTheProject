d4def5a9833143d8866061f2619acb3e98170122
Surribas1980 <issupl@gmail.com>
2022-04-07 13:48:42 +0200

    Modified - Footer.jsx - [ on : div ] - I've removed the div's style - src/componentes/elFooter/Footer.jsx

--- src/componentes/elFooter/Footer.jsx
+++ src/componentes/elFooter/Footer.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-
 function Footer() {
-
+  
   return (
   <div>
-    <div className="vh50 colorWhite fondoBlack"> El footer</div>
+    <div className="vh50 colorWhite fondoVerde"> El footer</div>
+    
   </div>
   );
 }
