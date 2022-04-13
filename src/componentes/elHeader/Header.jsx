66330eeb53ebf7b7e62a93783e2037fa678a7dc5
Surribas1980 <issupl@gmail.com>
2022-04-13 22:36:43 +0200

    Modified - Header.jsx -[useState] -I've introduced a div to click on mobile so I've modified when screen was less than 600px, and I've created a new state's variable also  - src/componentes/elHeader/Header.jsx

--- src/componentes/elHeader/Header.jsx
+++ src/componentes/elHeader/Header.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState,useEffect} from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
 import Login from '../../pages/Login';
 import Register from '../../pages/Register';
@@ -7,43 +7,61 @@ import NavBar from '../elNavBar/NavBar';
 export default function Header( props ,{children}) {
 
   const [toogleNavBar, setToogleNavBar] = useState(true);
+  const [botonMobile,setBotonMobil] = useState(true);
 
-  const bodyStyle = (styleBody) =>{
+
+    useEffect(()=>{
+
+    screen.width > 600 === true ? setBotonMobil(false) : '';
+    },[])
+
+
+
+  const bodyStyle = (styleBody,boton) =>{
     props.funChangeStyle(styleBody)
     styleBody === 'body-default' ? setToogleNavBar(true) : setToogleNavBar(false);
+    boton === botonMobile ? setBotonMobil(!boton) : '';
   }
 
   return (
     <>
       <nav id='navegation-default' className="borderColorBottom theRoboto ">
+        <div className="div-click-default" onClick={()=>{bodyStyle('body-default',botonMobile)}}>boton
+        </div>
         <div>
-          <Link onClick={()=>{bodyStyle('body-default')}} to="/">
+          <Link onClick={()=>{bodyStyle('body-default',false)}} to="/">
             Logo
           </Link>
         </div>
-        
-        <div>
 
-          { toogleNavBar === true ? <NavBar /> : ''}
-        </div>
-        <div id='navegation-default' >
+      { (botonMobile === false)  ?
+        <>
+              <div>
 
-          <div >
-            <Link onClick={()=>{bodyStyle('body-form-default') }} className="" to="/login">
-              Login
-            </Link>
-          </div>
-          <div>
-            <Link onClick={()=>{bodyStyle('body-form-default')}} to="/register">
-              Register
-            </Link>
-          </div>
-          <div>
-            <Link onClick={()=>{bodyStyle('body-default')}} to="/">
-              Main
-            </Link>
-          </div>
-        </div>
+                { toogleNavBar === true ? <NavBar /> : ''}
+              </div>
+
+
+              <div id='navegation-default' >
+
+                    <div >
+                      <Link onClick={()=>{bodyStyle('body-form-default',false) }} className="" to="/login">
+                        Login
+                      </Link>
+                    </div>
+                    <div>
+                      <Link onClick={()=>{bodyStyle('body-form-default',false)}} to="/register">
+                        Register
+                      </Link>
+                    </div>
+                    <div>
+                      <Link onClick={()=>{bodyStyle('body-default',false)}} to="/">
+                        Main
+                      </Link>
+                    </div>
+            </div>
+      </>
+       : ''}
       </nav>
       <Routes>
         <Route path={'/login'} element={<Login />}></Route>
