1d92853dd9f4bf2e36f7fcfae0fca0d8e32f001e
Surribas1980 <issupl@gmail.com>
2022-04-13 09:46:41 +0200

    Add - Footer.jsx - [ on div style ] - I've introduced a component, GenericBoton, I needed to have a button to go up the top and modified your style - src/componentes/elFooter/Footer.jsx

--- src/componentes/elFooter/Footer.jsx
+++ src/componentes/elFooter/Footer.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import {animateScroll as scroll} from 'react-scroll';
+import GenericBoton from '../GenericBoton';
 function Footer() {
-  
+  const Up = ()=>{
+    scroll.scrollToTop();
+  }
+  let styleButtom = "borderNone width5vw-change7 fondoAzulete  alignCenter theRoboto weight-letter-bold";
+  let styleDiv = "divFlex alignCenter justifyContentCenter toBottom0 positionStyke zIndexTest2 colorWhite fondoVerde";
   return (
  
-    <div className=" colorWhite fondoVerde"> El footer</div>
+    <>El footer
     
-  
+    <GenericBoton styleDiv={styleDiv} newFunction={Up} estilo={styleButtom} newForm={'boton from App'}/>
+    
+    </>
+    
+    
   );
 }
 
