4bc663137c9ab405bd1f1c9bc3b02786d6034e55
Surribas1980 <issupl@gmail.com>
2022-04-13 09:49:16 +0200

    Modified - App.js - [...lines...] - I've removed the component GenericBoton - src/App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import React,{useState} from 'react';
-import {animateScroll as scroll} from 'react-scroll';
 import UtilizoHook from './componentes/conHooks/UtilizoHook';
 import Header from './componentes/elHeader/Header';
-
 import Footer from './componentes/elFooter/Footer';
 import { AuthProvider } from './shared/context/authContex';
 
 import './style.css';
-import GenericBoton from './componentes/GenericBoton';
+
 
 export default function App() {
 const [styleBody, setStyleBody] = useState('body-default')
-  const Up = ()=>{
-    scroll.scrollToTop();
-  }
-  let styleButtom = "borderNone width5vw-change7 fondoAzulete  alignCenter theRoboto weight-letter-bold";
-  let styleDiv = "divFlex alignCenter justifyContentCenter"
+  
   return (
     <div className=''>
       <AuthProvider>
@@ -24,13 +18,10 @@ const [styleBody, setStyleBody] = useState('body-default')
           <Header funChangeStyle={setStyleBody} />
           <Footer />
         </div>
-          {/*<Footer />*/}
+          
         </UtilizoHook>
       </AuthProvider>
-      <div className='toBottom0 positionStyke zIndexTest2'>
-
-      <GenericBoton styleDiv={styleDiv} newFunction={Up} estilo={styleButtom} newForm={'boton from App'}></GenericBoton>
-      </div>
+      
      
     </div>
   );
