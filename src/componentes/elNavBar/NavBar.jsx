9a2f9800fec8262597799bd445a73e4b4b49ab9c
Surribas1980 <issupl@gmail.com>
2022-04-16 10:36:48 +0200

    Modified - NavBar.jsx -[on return]-I've removed severals divs and I use the frangmet only - src/componentes/elNavBar/NavBar.jsx

--- src/componentes/elNavBar/NavBar.jsx
+++ src/componentes/elNavBar/NavBar.jsx
@@ -3,47 +3,45 @@ import {Link} from 'react-scroll';
 
 function NavBar() {
   return (
-    <nav className='justifyContentCenter'>
-      <ul className="nav-items divFlex">
-        <li className="nav-item">
+    <>
+
         <Link activeClass="active"
-                    
+
                     to="seccion-1"
                     spy={true}
-                   
+
                     smooth={true}
                     duration={500}
                     offset={-50}>
         Section 1
-        </Link>  
-        </li>
-        <li className="nav-item">
+        </Link>
+
           <Link activeClass="active"
-                    
+
                     to="seccion-2"
                     spy={true}
-                    
+
                     smooth={true}
                     duration={500}
                     offset={-50}>
           Section 2
-          </Link> 
-        </li>
-        <li className="nav-item">
+          </Link>
+
+
           <Link activeClass="active"
-                   
+
                     to="seccion-3"
                     spy={true}
-                   
+
                     smooth={true}
                     duration={500}
                     offset={-50}>
           Section 3
           </Link>
-          </li>
-        
-      </ul>
-    </nav>
+
+
+
+    </>
   );
 }
 
