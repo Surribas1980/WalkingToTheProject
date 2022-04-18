bce079df8432c30bbfe756ba36d98a38d2b3bdbc
Surribas1980 <issupl@gmail.com>
2022-04-18 14:16:21 +0200

    Add - HeaderApp.jsx - [on Component] - I've introduced a new component to inform if I have new notices on the HeaderApp  - src/componentes/HeaderApp.jsx

--- src/componentes/HeaderApp.jsx
+++ src/componentes/HeaderApp.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import useAuth from '../shared/hooks/useAuth';
-
+import Salir from '../images/menu/salida.png';
 export default function HeaderApp({ children }) {
   const { logOut } = useAuth();
+  console.log('el children: ',children)
   return (
-    <>
-      <button
+    <div id="navegation-main-app">
+      <img src={Salir} className="tamanio" id='img-header-default'
         onClick={() => {
           logOut();
         }}
-      >
-        Salir
-      </button>
-    </>
+      />
+
+
+      <div>{children}</div>
+    </div>
   );
 }
