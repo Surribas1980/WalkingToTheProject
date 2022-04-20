69bc039968ffd088eb7d066b780a371acafcb1c2
Surribas1980 <issupl@gmail.com>
2022-04-20 22:01:57 +0200

    Modified and Add - UtilizoHook.jsx - [...lines...] - I've modified this component because I went to see the header and footer of app on the same view  - src/componentes/elHeader/HeaderIndexApp.jsx

--- src/componentes/conHooks/UtilizoHook.jsx
+++ src/componentes/conHooks/UtilizoHook.jsx
@@ -1,23 +1,19 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-
-/*import Usuarios from '../../pages/Usuarios';*/
-import MainApp from '../../pages/MainApp';
-import MyServicesM from '../../pages/MyServices/MyServicesM';
-import Services from '../../pages/Services/Services';
 import useAuth from '../../shared/hooks/useAuth';
+
+import FooterIndexApp from '../elFooter/FooterIndexApp';
+import HeaderIndexApp from '../elHeader/HeaderIndexApp';
+import RutasApp from '../lasRutasApp/RutasApp';
 
 export default function UtilizoHook({ children }) {
   const { user } = useAuth('');
   return (
     <>
-      {user === 'logeado' ? (
-
-        <Routes>
-          <Route path={'/mainapp'} element={<MainApp />}/>
-          <Route path={'/myservices'} element={<MyServicesM />}/>
-          <Route path={'/services'} element={<Services />}/>
-        </Routes>
+      {user === 'logeado' ? (<>
+        <HeaderIndexApp />
+        <RutasApp />
+        <FooterIndexApp />
+        </>
       ) : (
         children
       )}
