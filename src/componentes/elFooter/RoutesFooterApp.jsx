d495bfa15c5b7849230b1c6e59f4a3df221b0588
Surribas1980 <issupl@gmail.com>
2022-04-20 22:07:10 +0200

    Add - RoutesFooterApp.jsx - [ New Component ] - I've created this component to link, it's react-router-dom  - src/componentes/elFooter/RoutesFooterApp.jsx

--- src/componentes/elFooter/RoutesFooterApp.jsx
+++ src/componentes/elFooter/RoutesFooterApp.jsx
@@ -1,8 +1,6 @@
 import React from "react";
-import {Routes, Route, Link, Outlet} from "react-router-dom";
-import MainApp from "../../pages/MainApp";
-import MyServicesM from "../../pages/MyServices/MyServicesM";
-import Services from "../../pages/Services/Services";
+import {Link} from "react-router-dom";
+
 import Usuarios from '../../images/menuApp_Footer/grupo-de-usuarios.png';
 import Usuario from '../../images/menuApp_Footer/perfil.png';
 import Home from '../../images/menuApp_Footer/hogar.png';
@@ -11,10 +9,9 @@ function RoutesFooterApp(){
     return(
     <>
     <div id="navegation-footer-app" className="ocultarDatos">
-        <Link to="/myservices"><img src={Usuario} alt="enlace user"/></Link>
-        <Link to="/services"><img src={Usuarios} alt="usuarios"/></Link>
-        
-        <Link to="/mainapp"><img src={Home} alt="Home"/></Link>
+        <Link to="/myservices"><img className="img-nav-footer-app" src={Usuario} alt="enlace user"/></Link>
+        <Link to="/services"><img className="img-nav-footer-app" src={Usuarios} alt="usuarios"/></Link>
+        <Link to="/mainapp"><img className="img-nav-footer-app" src={Home} alt="Home"/></Link>
     </div>
 
     </>
