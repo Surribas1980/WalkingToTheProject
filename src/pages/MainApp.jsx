4617fa8ffc921116c96c54aa521eb61dc8f4c655
Surribas1980 <issupl@gmail.com>
2022-04-20 22:10:53 +0200

    Modified and removed - MainApp.jsx - [...lines...] - I've removed severals components,header and footer - src/pages/MainApp.jsx

--- src/pages/MainApp.jsx
+++ src/pages/MainApp.jsx
@@ -4,21 +4,16 @@ import Usuarios from './Usuarios';
 import UnaSeccion from '../componentes/lasSecciones/UnaSeccion';
 import DosSeccion from '../componentes/lasSecciones/DosSeccion';
 import TresSeccion from '../componentes/lasSecciones/TresSeccion';
-import HeaderApp from '../componentes/HeaderApp';
-import Notices from '../componentes/losAvisos/Notices';
+
 import Info from '../componentes/lasSecciones/infoUnaSecion/Info';
 import ChartRanking from '../componentes/lasSecciones/infoDosSeccion/ChartRanking';
-import FooterApp from '../componentes/elFooter/FooterApp';
-import RoutesFooterApp from '../componentes/elFooter/RoutesFooterApp';
-import LinksFooterApp from '../componentes/elFooter/LinksFooterApp';
+
 function MainApp(){
 
 
   return (
     <>{console.time('loop')}
-    <HeaderApp>
-      <Notices />
-    </HeaderApp>
+   
 
     <UnaSeccion>
 
@@ -31,10 +26,7 @@ function MainApp(){
       el children desde MainApp en TresSeccion
       <Usuarios />
     </TresSeccion>
-    <FooterApp>
-      {/*<LinksFooterApp />*/}
-      <RoutesFooterApp />
-    </FooterApp>
+
 {console.timeEnd('loop')}
     </>);
 
