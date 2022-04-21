ad8e0cb3cbdb6955f6cd296aaa0703aa3a2e9aff
Surribas1980 <issupl@gmail.com>
2022-04-21 22:24:47 +0200

    Modified - LinksFooterApp.jsx -[on div] - I've introduced a new Link taskList - FRONT/src/componentes/elFooter/LinksFooterApp.jsx

--- FRONT/src/componentes/elFooter/LinksFooterApp.jsx
+++ FRONT/src/componentes/elFooter/LinksFooterApp.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
+
 import Usuarios from '../../images/menuApp_Footer/grupo-de-usuarios.png';
 import Usuario from '../../images/menuApp_Footer/perfil.png';
 import Home from '../../images/menuApp_Footer/hogar.png';
+import Lista from '../../images/menuApp_Footer/lista-de-tareas.png';
 function LinksFooterApp(){
 
     return(
-    <div id="navegation-footer-app" className="ocultarDatos">
-    <Link to="/user"><img src={Usuario} alt="enlace user"/></Link>
-    <Link to="/services"><img src={Usuarios} alt="usuarios"/></Link>
-    <Link to="/mainapp"><img src={Home} alt="Home"/></Link>
+        <div id="navegation-footer-app" className="ocultarDatos">
+        <Link to="/myservices"><img className="img-nav-footer-app" src={Usuario} alt="enlace user"/></Link>
+        <Link to="/services"><img className="img-nav-footer-app" src={Usuarios} alt="usuarios"/></Link>
+        <Link to="/mainapp"><img className="img-nav-footer-app" src={Home} alt="Home"/></Link>
+        <Link to="/tasklist"><img className="img-nav-footer-app" src={Lista} alt="Home"/></Link>
+    
     </div>)
 }
 
