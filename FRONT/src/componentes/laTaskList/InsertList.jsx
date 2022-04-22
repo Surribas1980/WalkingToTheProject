19e3d57d17035bbee5ea3e89cecec9ec620f2afd
Surribas1980 <issupl@gmail.com>
2022-04-22 13:38:29 +0200

    Modified -[...lines...] -I've removed several components and I've used 'children' - FRONT/src/componentes/laTaskList/InsertList.jsx

--- FRONT/src/componentes/laTaskList/InsertList.jsx
+++ FRONT/src/componentes/laTaskList/InsertList.jsx
@@ -1,25 +1,11 @@
 import React,{useState,useEffect} from "react";
-import Delete from '../../images/tasklist/bote-de-basura.png';
-import PaintList from "./PaintList";
-import SendList from "./SendList";
-function InsertList(props){
-    const [lista,setLista] = useState([]);
-    const [cambia,setCambia] = useState(false);
+
+function InsertList({children}){
     
-   
-    useEffect(()=>{},[cambia])
-    let cam;
-    const SaveList = (dato)=>{
-        lista.push(dato);
-        props.valorInput.current.value= '';
-        
-        cam = cambia;
-        setCambia(!cam)
-    }
-    return (<>
-    <PaintList list={lista} imgEliminar={Delete}/>
-    <button onClick={()=>{SaveList(props.valorInput.current.value)}}>Guardar</button>
-    <SendList />
+  
+    return (<>{children}
+    
+  
     </>)
 }
 
