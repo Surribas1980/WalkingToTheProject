44ebb42c6a489993860b03318af68284b410527f
Surribas1980 <issupl@gmail.com>
2022-04-22 22:33:01 +0200

    Modified - Icono.jsx - [name] - I've introduce the atribute name on img  - FRONT/src/componentes/Icono.jsx

--- FRONT/src/componentes/Icono.jsx
+++ FRONT/src/componentes/Icono.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Icono(props){
-    return (<img onClick={(event)=>{props.eliminarFun(event,props.elkey)}} id="icono" src={props.imagen} alt="imagen"/>)
+    return (<img name={props.elname} onClick={(event)=>{props.eliminarFun(event,props.elkey)}} id="icono" src={props.imagen} alt="imagen"/>)
 }
 
 export default Icono;
