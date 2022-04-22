46cca8626d94be4b2b8a501480ea17fb56d30434
Surribas1980 <issupl@gmail.com>
2022-04-22 13:49:56 +0200

    Modified - PaintList.jsx - [] - I've removed useState and useEffect  - FRONT/src/componentes/lasTaskList/PaintList.jsx

--- FRONT/src/componentes/laTaskList/PaintList.jsx
+++ FRONT/src/componentes/laTaskList/PaintList.jsx
@@ -1,4 +1,4 @@
-import React,{useState,useEffect} from "react";
+import React from "react";
 import Icono from "../Icono";
 import Delete from '../../images/tasklist/bote-de-basura.png';
 function PaintList(props){
