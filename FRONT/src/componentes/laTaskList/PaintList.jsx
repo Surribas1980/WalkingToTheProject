8ed7bf162e91747dc1b6250356d083c65a459700
Surribas1980 <issupl@gmail.com>
2022-04-22 22:29:51 +0200

    Modified - PaintList.jsx - [...lines...] - I've introduced several functions and styles  - FRONT/src/componentes/laTaskList/PaintList.jsx

--- FRONT/src/componentes/laTaskList/PaintList.jsx
+++ FRONT/src/componentes/laTaskList/PaintList.jsx
@@ -1,17 +1,52 @@
-import React from "react";
+import React,{useState} from "react";
 import Icono from "../Icono";
 import Delete from '../../images/tasklist/bote-de-basura.png';
+import TareaRealizada from '../../images/tasklist/ok-marca.png';
 function PaintList(props){
-    
+    const [color,setColor] = useState([]);/*"tasklist-element-statuss-change"*/
+    const [cambiaStyle,setCam] = useState(false);
+
+
     const eliminar = (event,valor)=>{
         console.log('event: ', event,valor)
     }
+
+   
+
+    props?.list.map(()=>{
+        color.push("tasklist-element");
+        return 1
+    })
+   
     const salida = props?.list.map((item,index)=>{
+        
+        const paintGreen = (cambiarAotroEstilo,elemmento,elEstiloDelDiv) =>{
+            let cambia;
+            if(elEstiloDelDiv === "tasklist-element"){
+
+                let nuevoStilo = ' tasklist-element-statuss-change';
+                cambia = `tasklist-element${nuevoStilo}`;
+                color[elemmento] = cambia;
+                setCam(cambiarAotroEstilo)
+            }else{
+                
+                color[elemmento] = "tasklist-element";
+                setCam(cambiarAotroEstilo)
+                
+            }
+            
+            
+        }
+
+        
         return (
         <div className="tasklist">
-            <div className="tasklist-element" >{item}</div>
-            <Icono eliminarFun={eliminar} elkey={index} imagen={Delete}/>
-        </div>)
+            <div className={color[index]} onClick={()=>{paintGreen(!cambiaStyle,index,color[index])}}>
+                <span className="span-tarea"><Icono elname={"ok"} eliminarFun={eliminar} elkey={index} imagen={TareaRealizada}/></span>{item}
+            </div>
+            <Icono elname={"delete"} eliminarFun={eliminar} elkey={index} imagen={Delete}/>
+        </div>
+        )
     })
 
    
