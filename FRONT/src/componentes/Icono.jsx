5d525c10ebdcc5dbd8b452264015394f3fe0db66
Surribas1980 <issupl@gmail.com>
2022-04-25 07:28:41 +0200

    Modified - Icono.jsx - [on img] - I've introduced a new attribute name - FRONT/src/componentes/Icono.jsx

--- FRONT/src/componentes/Icono.jsx
+++ FRONT/src/componentes/Icono.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 
 function Icono(props){
+    
+   
     return (<img name={props.elname} onClick={(event)=>{props.eliminarFun(event,props.elkey)}} id="icono" src={props.imagen} alt="imagen"/>)
 }
 
