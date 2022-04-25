c3618f4dcc0fda9b5d7a8803944fa7a9b1a60226
Surribas1980 <issupl@gmail.com>
2022-04-25 11:52:47 +0200

    Modified - Icono.jsx - [on img] - I've modified the function   - FRONT/src/componentes/laTaskList/Input.jsx

--- FRONT/src/componentes/Icono.jsx
+++ FRONT/src/componentes/Icono.jsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React,{memo} from "react";
 
 function Icono(props){
     
    
-    return (<img name={props.elname} onClick={(event)=>{props.eliminarFun(event,props.elkey)}} id="icono" src={props.imagen} alt="imagen"/>)
+    return (<img name={props.elname} onClick={(event)=>{props.eliminarFun(props.elkey)}} id="icono" src={props.imagen} alt="imagen"/>)
 }
 
-export default Icono;
+export default memo(Icono);
