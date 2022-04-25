1db710d6a86b61a0ef953570be7a40e2d151589d
Surribas1980 <issupl@gmail.com>
2022-04-25 07:19:39 +0200

    Add - WritenTitleOrList.jsx - [New Component] - This component is used to write the title or the task  - FRONT/src/componentes/laTaskList/WritenTitleORList.jsx

---
+++ FRONT/src/componentes/laTaskList/WritenTitleOrList.jsx
@@ -0,0 +1,12 @@
+import React, { memo } from "react";
+
+function WritenTitleOrList (props){
+return(
+<div className="" id="writinglist">
+    <span>{props.elspan}</span>
+    <input ref={props.inputref}  placeholder={props.elplace}/>
+    <button className="save-data-default" onClick={()=>{props.laFun(props.inputref.current.value)}}>Guardar</button>
+</div>)
+}
+
+export default memo(WritenTitleOrList);
