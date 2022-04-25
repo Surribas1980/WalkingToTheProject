a2b7674874a7a3ed572e7c465f48885c0981038a
Surribas1980 <issupl@gmail.com>
2022-04-25 07:25:45 +0200

    Add and Modified - [...lines...] - I've introduced a new componente and I've used useCallback for two functions - FRONT/src/pages/TaskList.jsx

--- FRONT/src/pages/TaskList.jsx
+++ FRONT/src/pages/TaskList.jsx
@@ -1,33 +1,59 @@
-import React, {useState,useRef,useEffect} from "react";
+import React, {useState,useRef,useEffect, useCallback} from "react";
 import InsertList from "../componentes/laTaskList/InsertList";
 import PaintList from "../componentes/laTaskList/PaintList";
 import SendList from "../componentes/laTaskList/SendList";
-import WritingList from "../componentes/laTaskList/WritingList";
+import TitleList from "../componentes/laTaskList/TitleList";
+import WritenTitleOrList from "../componentes/laTaskList/WritenTitleOrList";
+
 
 function TaskList(){
-    const [cambia,setCambia] = useState(false);
+    
     const [lista,setLista] = useState([]);
+    let [titleListTask , setTitleListTask ] = useState("Lista de tareas")
+   
     const valueInput = useRef("");
+    const valueTitle = useRef("");
 
+    
+    const SaveList = useCallback ((dato)=>{
+        
+        setLista(()=>{
+            const novoObj = new Object();
+            
+                novoObj.task = dato;
+                novoObj.done = 'not done';
+                valueInput.current.value= '';
+            
+            lista.push(novoObj)
+            
+            return [...lista]
+        });
 
-   
-    useEffect(()=>{},[cambia])
-    const SaveList = (dato)=>{
-        let cam;
-        lista.push(dato);
-        valueInput.current.value= '';
-        cam = cambia;
-        setCambia(!cam)
-    }
-   
+        console.log('dentro',lista);
+        
+    },[lista]);
+    
+    
+    const SaveTitleList = useCallback ((titulo) =>{
+        setTitleListTask(titulo)
+        valueTitle.current.value= '';
+        
+        let novoObj = new Object();
+        novoObj.title = titulo;
+        lista.push(novoObj);
+    },[titleListTask]);
 
     return(<>
-    <div name="titulo-lista-tareas">Lista de tareas</div>
+   
+    
+    <TitleList title={titleListTask}/>
     <PaintList list={lista} />
     <InsertList>
-        <WritingList laref={valueInput} laFun={SaveList}/>
+       
+        <WritenTitleOrList elspan="Tarea" inputref={valueInput}  laFun={SaveList} elplace="Introduce tarea ..."/>
+        <WritenTitleOrList elspan="Cambio de titulo?" inputref={valueTitle} laFun={SaveTitleList} elplace="Introduce titulo de tarea ..."/>
     </InsertList>
-    <SendList />
+    <SendList list={lista} />
     </>)
 }
 
