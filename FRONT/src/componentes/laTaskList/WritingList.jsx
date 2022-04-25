69a646dbcad84a37a630c727a99068c2b4d002a8
Surribas1980 <issupl@gmail.com>
2022-04-25 07:44:37 +0200

    I HAVE TO DELETE THIS COMPONENT - WritingList.jsx - FRONT/src/componentes/laTaskList/WritingList.jsx

--- FRONT/src/componentes/laTaskList/WritingList.jsx
+++ FRONT/src/componentes/laTaskList/WritingList.jsx
@@ -5,8 +5,9 @@ function WritingList(props){
     return(
     <div className="" id="writinglist">
         <span>Tarea</span>
-        <input ref={props.laref}  placeholder="Introduce tarea ..."/>
-        <button className="save-data-default" onClick={()=>{props.laFun(props.laref.current.value)}}>Guardar</button>
+        <input ref={props.inputref}  placeholder="Introduce tarea ..."/>
+        <button className="save-data-default" onClick={()=>{props.laFun(props.inputref.current.value)}}>Guardar</button>
+        
         
     </div>)
 }
