a472fba79493dd802a6b468d4723c8bef07659e2
Surribas1980 <issupl@gmail.com>
2022-04-27 11:18:16 +0200

    Modified and add - Info.jsx - [on div] - I've introduced a new element - Link- ,with a new style 'name='  - FRONT/src/componentes/lasSecciones/infoUnaSecion/Info.jsx

--- FRONT/src/componentes/lasSecciones/infoUnaSecion/Info.jsx
+++ FRONT/src/componentes/lasSecciones/infoUnaSecion/Info.jsx
@@ -1,31 +1,42 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+
 import Docs from '../../../images/UnaSeccion/docs.png';
 import DocsOk from '../../../images/UnaSeccion/docsOk.png';
 import Folder from '../../../images/UnaSeccion/folder.png';
 function Info(){
   return(
     <>{console.time('loop')}
-    <div id='info-position-relative'>
-      <div id="info">
-        <img src={Docs} alt="Imagen de docs"/>
-        <span>Nº de mis servicios</span>
-      </div>
+    <Link name="info" to="/myservices">
+      <div id='info-position-relative'>
+        <div id="info">
+            <img src={Docs} alt="Imagen de docs"/>
+            <span>Nº de mis servicios</span>
+        </div>
 
-    </div>
-    <div id='info-position-relative'>
-
-      <div id="info">
-        <img src={DocsOk} alt="Imagen de docsOk"/>
-        <span>Nº de mis servicios solucionados</span>
       </div>
-    </div>
-    <div id='info-position-relative'>
+    </Link>
+    <Link name="info" to="/myservicessolutioned">
+        
+      <div id='info-position-relative'>
 
-      <div id="info">
-        <img src={Folder} alt="Imagen de folders"/>
-        <span>Total sin resolver</span>
+        <div id="info">
+            <img src={DocsOk} alt="Imagen de docsOk"/>
+            <span>Nº de mis servicios solucionados</span>
+        </div>
       </div>
-    </div>
+    </Link>
+
+    <Link name="info" to="/services">
+        
+      <div id='info-position-relative'>
+        <div id="info">
+            <img src={Folder} alt="Imagen de folders"/>
+            <span>Total sin resolver</span>
+        </div>
+      </div>
+    </Link>
+
       {console.timeEnd('loop')}
     </>)
 }
