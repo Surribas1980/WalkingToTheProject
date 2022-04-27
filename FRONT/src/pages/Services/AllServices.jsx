a3f608024715c7bb6e1d0bf43cbc6bcc3a781947
Surribas1980 <issupl@gmail.com>
2022-04-27 22:41:38 +0200

    Modified - AllServices.jsx -[on div] - I've introduced several divs to show information about if the service is solutioned or not solutioned  - src/pages/Services/AllServices.jsx

--- FRONT/src/pages/Services/AllServices.jsx
+++ FRONT/src/pages/Services/AllServices.jsx
@@ -1,7 +1,15 @@
 import React,{useState} from "react";
 
 function AllServices({children}){
-    return(<>{children}</>)
+    return(<>
+      <div className="etiquetas">
+        <div className="etiquetas-colores">
+          <div className="etiquetas-colores-red">No solucionados</div>
+          <div className="etiquetas-colores-green">Solucionados</div>
+        </div>
+      </div>
+      {children}
+      </>)
 }
 
 export default AllServices;
