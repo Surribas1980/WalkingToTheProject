7aa7e9f47e46650f135529bf8f64651b2f6c0017
Surribas1980 <issupl@gmail.com>
2022-04-27 13:36:44 +0200

    Modified - Services.jsx - [...lines...] - I've introduced new components and several functions, with useState also  - FRONT/src/pages/Services/Services.jsx

--- FRONT/src/pages/Services/Services.jsx
+++ FRONT/src/pages/Services/Services.jsx
@@ -1,11 +1,33 @@
-import React from "react";
+import React,{useState} from "react";
 import ListMessages from "./ListMessages";
-
+import AllServices from "./AllServices";
+import { getAllServices } from "../../data.js";
 function Services(){
+    const [services,setServices] = useState(getAllServices());
+    const [solucionados,setSolucionados] = useState([]);
+    const [noSolucionados,setNoSolucionados] = useState([]);
+
+    let elvector = services?.map((item,index)=>{
+        let elemento = {};
+        elemento.titulo = item.solutioned;
+        return (elemento)
+    })
+    let solu = services.filter((item)=>{
+        return (item.solutioned === true);
+    })
+    let nosolu = services.filter((item)=>{
+        return (item.solutioned === false);
+    })
+console.log('services', elvector,solu,nosolu)
     return(<>
     <ListMessages>
         Esto es desde Services a ListMessages
     </ListMessages>
+    <div id="total-services">
+        <AllServices>
+            
+        </AllServices>
+    </div>
     </>)
 }
 
