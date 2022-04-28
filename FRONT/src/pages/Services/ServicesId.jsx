2d3ca3854064dea0df7265b61a8733720506608b
Surribas1980 <issupl@gmail.com>
2022-04-28 13:50:09 +0200

    Modified - [...lines...] - I've introduced new component  -  FRONT/src/pages/Services/ServicesId.jsx

--- FRONT/src/pages/Services/ServicesId.jsx
+++ FRONT/src/pages/Services/ServicesId.jsx
@@ -1,9 +1,25 @@
-import React,{useState} from 'react';
+import React from 'react';
 import { useParams } from "react-router-dom";
+import ServicesTasksId from '../../componentes/losServicesId/ServicesTasksId.jsx';
+
+import {getAllServices} from '../../data.js';
+
 function ServicesId(){
   let params = useParams();
-  console.log(params.id)
-  return (<div><h1>Estoy en servicios id: {params.id}</h1></div>)
+  /*const [salida,setSalida] = useState([])*/
+  let salida = getAllServices()?.filter((item)=>{
+    let dato;
+    if(item.id == params.id){
+       dato = item;
+    }
+    return (dato);
+  })
+  console.log('estoy en ServiciosId: ',salida)
+  return (
+  <div>
+    <h1>Estoy en servicios id: {params.id}</h1>
+      <ServicesTasksId taskId={salida} />
+    </div>)
 }
 
 export default ServicesId;
