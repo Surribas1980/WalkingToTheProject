840904dd7317c7b3e2e0d205cfea828ecf80782d
Surribas1980 <issupl@gmail.com>
2022-04-30 12:23:06 +0200

    Withou Changes

--- FRONT/src/componentes/lasRutasApp/RutasApp.jsx
+++ FRONT/src/componentes/lasRutasApp/RutasApp.jsx
@@ -12,8 +12,6 @@ function RutasApp(){
         <Route path={'/mainapp'} element={<MainApp />}/>
         <Route path={'/myservices'} element={<MyServicesM />}/>
         <Route path={'/services'} element={<Services />}/>
-
-        
         <Route path={'/services/:id'} element={<ServicesId />} />
         <Route path={'/tasklist'} element={<TaskList />} />
         <Route path={'/myservicessolutioned'} element={<Solutioned />} />
