0f247aa7a494e10eb71730f4fba8ce44af58a9bc
Surribas1980 <issupl@gmail.com>
2022-04-30 12:26:18 +0200

    Modified - ServicesFilesTasksId.jsx - I've introduced diferents lines to show the files with yours images  - src/componentes/losServicesId/ServicesFilesTasksId.jsx

--- FRONT/src/componentes/losServicesId/ServicesFilesTasksId.jsx
+++ FRONT/src/componentes/losServicesId/ServicesFilesTasksId.jsx
@@ -1,7 +1,47 @@
-import React from "react";
-
+import React,{useState} from "react";
+import Docx from '../../images/documents/archivo-word.png';
+import Xlsx from '../../images/documents/archivo-xlsx.png';
+import Pptx from '../../images/documents/archivo-powerpoint.png';
 function ServicesFilesTasksId(props){
-    return (<></>)
+const [documento,setDocumento] = useState([Object.values(props.ficheros)]);
+
+
+  let salida = documento[0].map((item,index)=>{
+    let thePath;
+    const urlstatic = "/FRONT/src/documentos/";
+
+    if(item.slice(-4) == "docx"){
+      thePath = Docx;
+
+    }
+    if(item.slice(-4) == "xlsx"){
+      thePath = Xlsx;
+    }
+    if(item.slice(-4) == "pptx"){
+      thePath = Pptx;
+    }
+    let dir = urlstatic + `${item}`;
+    let enlazar = dir.slice(14);
+    return (
+      <div>
+        <img id="icono" src={thePath}/>
+        <a href={dir}>
+          <span name="task">{item}</span>
+        </a>
+      </div>
+    )
+  })
+
+    return (<>
+      <div id="services-task-id">
+      <span name="service-titulo">Archivos:</span>
+        <div>
+          <div>
+            {salida}
+          </div>
+        </div>
+      </div>
+    </>)
 }
 
 export default ServicesFilesTasksId;
