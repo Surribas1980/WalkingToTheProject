a7dd855fa018972e25c1eaab1c9331c34947fb8f
Surribas1980 <issupl@gmail.com>
2022-04-30 12:27:13 +0200

    Withou Changes

--- FRONT/src/componentes/losServicesId/ServicesTasksId.jsx
+++ FRONT/src/componentes/losServicesId/ServicesTasksId.jsx
@@ -1,37 +1,29 @@
 import React,{useState} from "react";
 import ServicesTasksTaskId from "./ServicesTasksTaskId";
+import ServicesFilesTasksId from "./ServicesFilesTasksId";
 
 function ServicesTasksId(props){
     let ele = props.taskId;
-    let salida2 = ele.map((item,index)=>{
-      return(<>{item.titulo} {item.descripcion} {item.usuario} {item.tasks}</>)
-    })
+
     let salida = ele.map((item,index)=>{
 
 
         return(
         <>
-<<<<<<< HEAD
-            <div name="service-titulo">{item.titulo}</div>
-            <div name="service-description">{item.descripcion}</div>
-            <div name="service-usuario">{item.usuario}</div>
-
-        </>)
-    })
-    console.log('estoy en ServicesTasksId salida:',salida,ele.tasks,'- salida2:',salida2)
-=======
             <span name="service-titulo">Título: {item.titulo}</span>
             <span name="service-description">Descripción: {item.descripcion}</span>
             <span name="service-usuario">Usuario: {item.usuario}</span>
-         
+
         </>)
     })
-    
->>>>>>> cf6ebabc526ea6bbaae74f6804d7807dfe8a44fa
+
     return (
     <div id="services-task-id">
         {salida}
         <ServicesTasksTaskId task={props.taskId[0].tasks}/>
+
+        <ServicesFilesTasksId ficheros={props.taskId[0].files}/>
+
     </div>);
 }
 
