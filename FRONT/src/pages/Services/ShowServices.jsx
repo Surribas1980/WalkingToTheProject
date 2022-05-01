645753be1667b0f0f481a28de9743802f23f37ca
Surribas1980 <issupl@gmail.com>
2022-05-01 10:14:10 +0200

    Modified - ShowServices.jsx - I've used a function .slice for the array   - FRONT/src/pages/Services/ShowServices.jsx

--- FRONT/src/pages/Services/ShowServices.jsx
+++ FRONT/src/pages/Services/ShowServices.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import {Link,Outlet} from "react-router-dom";
+import imagen from '../../images/avatares/chico.png';
 function SolutionedServices(props){
 
 let salida = props.servicios.map((item,index)=>{
+  let sortDescription = item.descripcion.slice(0,20);
   return (
     <Link name="service-defalut" to={`/services/${item.id}`} key={index}>
       <div className={props.styleShowServices}>
-        <span name="service-titulo">Titulo: {item.titulo}</span>
-        <span name="service-usuario">Usuario: {item.usuario}</span>
-        <span name="service-description">Descripción: {item.descripcion}</span>
+        <img id='icono' name="services" className="show-services-list-span" src={imagen}/>
+        <div className="show-services-list-span">
+          <span name="service-titulo">Titulo: {item.titulo}</span>
+          <span name="service-usuario">Usuario: {item.usuario}</span>
+          <span name="service-description-initial">Descripción: {sortDescription}(...)</span>
+        </div>
+
       </div>
     </Link>
   )
