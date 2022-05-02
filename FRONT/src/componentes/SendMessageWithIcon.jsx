d624af773ba81e40081880d264d75139664fe243
Surribas1980 <issupl@gmail.com>
2022-05-02 13:55:08 +0200

    Modified - SendMessagesWithIcon.jsx - I've introuced new components  - FRONT/src/componentes/SendMessageWithIcon.jsx

--- FRONT/src/componentes/SendMessageWithIcon.jsx
+++ FRONT/src/componentes/SendMessageWithIcon.jsx
@@ -2,7 +2,8 @@ import React,{useState} from 'react';
 import GenericImageOnClick from './GenericImageOnClick';
 import GenericFormTextArea from './GenericFormTextArea';
 import Conversacion from '../images/conversacion.png';
-
+import Solucionado from '../images/think.png';
+import GenericBoton from './GenericBoton';
 function SendMessageWithIcon(){
 const [ver,setVer] = useState(false);
 
@@ -13,8 +14,15 @@ const changedVer = ()=>{
 }
   return (
     <>
+    <div className="services-task-id-group">
+      <GenericBoton styleDiv="services-button"  texto="Ver solución"/>
       <GenericImageOnClick idDivImg="generic-image-on-click" srcImg={Conversacion} functionImg={changedVer} />
+      <GenericImageOnClick idDivImg="generic-image-on-click"  srcImg={Solucionado} functionImg={changedVer} />
+    </div>
+     <div>
+
       {ver === true ? <GenericFormTextArea idDivForm="generic-form-text-area" /> : ''}
+     </div>
     </>
   )
 }
