083c01609f9bcada179638067db66fb6ae33ba9f
Surribas1980 <issupl@gmail.com>
2022-05-03 13:50:38 +0200

    Modified and Add - WriteTitleOrList.jsx - I've modified this component to show others - FRONT/src/componentes/laTaskList/WritenTitleOrList.jsx

--- FRONT/src/componentes/laTaskList/WritenTitleOrList.jsx
+++ FRONT/src/componentes/laTaskList/WritenTitleOrList.jsx
@@ -1,10 +1,21 @@
 import React, { memo } from "react";
 
 function WritenTitleOrList (props){
+    
+    let salida;
+    if(props.eltype == "text"){
+        salida = <><input type={props.eltype} ref={props.inputref}  placeholder={props.elplace}/></>;
+    }else if(props.eltype == "file"){
+        console.log('types: ',props.eltype)
+        salida =<><input multiple type={props.eltype} ref={props.inputref}  placeholder={props.elplace}/></>;
+    }else if(props.eltype == "textarea"){
+        salida =<><textarea type={props.eltype} rows="10" cols="50"  ref={props.inputref}  placeholder={props.elplace}/></>;
+    }
 return(
 <div className="" id="writinglist">
     <span>{props.elspan}</span>
-    <input type={props.eltype} ref={props.inputref}  placeholder={props.elplace}/>
+    
+    {salida}
     <button className="save-data-default" onClick={()=>{props.laFun(props.inputref.current.value)}}>Guardar</button>
 </div>)
 }
