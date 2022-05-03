5f99f1076a9dc2eefeae78043cbd90704fd0ddce
Surribas1980 <issupl@gmail.com>
2022-05-03 13:48:39 +0200

    Modified and Add - [...lines...] - I've modified this component with useState to use other components   - FRONT/src/componentes/laTaskList/PaintList.jsx

--- FRONT/src/componentes/laTaskList/PaintList.jsx
+++ FRONT/src/componentes/laTaskList/PaintList.jsx
@@ -2,12 +2,18 @@ import React,{memo,useState} from "react";
 import Icono from "../Icono";
 import Delete from '../../images/tasklist/bote-de-basura.png';
 import TareaRealizada from '../../images/tasklist/ok-marca.png';
+import PaintFiles from "./PaintFiles";
+import PaintDescription from "./PaintDescription";
 
 function PaintList(props){
     const [color,setColor] = useState([]);/*"tasklist-element-statuss-change"*/
     const [cambiaStyle,setCam] = useState(false);
+    
+
+    /*console.log('props?.list[0]: ',props?.list[0],props?.list.length, props?.list[2].fichero)*/
 
 
+    
     props?.list.map(()=>{
         color.push("tasklist-element");
         return 1
@@ -37,21 +43,39 @@ function PaintList(props){
             
         }
 
+        let texto;
+        console.log('tasks...',item.task,item)
+        if(item.task){
+            texto = <>
+
+            <div className="tasklist">
+                <div className={color[index]} onClick={()=>{paintGreen(!cambiaStyle,index,color[index])}}>
+                    <span className="span-tarea"><Icono elname={"ok"}  elkey={null} imagen={TareaRealizada}/></span>
+                    {item.task}
+                </div>
+                <Icono elname={"delete"} eliminarFun={props.laFun} elkey={index} imagen={Delete}/>
+            </div>
+           
+        </>;
+
+        }
         
         return (
-        <div className="tasklist">
-            <div className={color[index]} onClick={()=>{paintGreen(!cambiaStyle,index,color[index])}}>
-                <span className="span-tarea"><Icono elname={"ok"}  elkey={null} imagen={TareaRealizada}/></span>{item.task}
-            </div>
-            <Icono elname={"delete"} eliminarFun={props.laFun} elkey={index} imagen={Delete}/>
-        </div>
+            <>{texto}</>
         )
     })
 
-   
+    
    
 
-    return(<div className="totaltasklist">{salida}</div>)
+    
+
+    return(
+    <div className="totaltasklist">
+        {salida}
+        { props.thereFiles === true ? <PaintFiles lista={props?.list}/> : ''}
+        { props.thereDescription === true ? <PaintDescription list={props?.list}/> : ''}
+    </div>)
 }
 
 export default  memo(PaintList);
