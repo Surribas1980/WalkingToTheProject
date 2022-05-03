b0c7de9e78093cf5dde37e7d5f721bfa407a0f87
Surribas1980 <issupl@gmail.com>
2022-05-03 07:49:07 +0200

    Modified - ServicesTasksId.jsx -[...lines...] - I've introduced severals divs  - FRONT/src/componentes/losServicesId/ServicesTasksId.jsx

--- FRONT/src/componentes/losServicesId/ServicesTasksId.jsx
+++ FRONT/src/componentes/losServicesId/ServicesTasksId.jsx
@@ -4,7 +4,7 @@ import ServicesFilesTasksId from "./ServicesFilesTasksId";
 import SendMessageWithIcon from "../SendMessageWithIcon";
 function ServicesTasksId(props){
     let ele = props.taskId;
-
+    console.log(ele[0]?.solutioned,ele)
     let salida = props.taskId?.map((item,index)=>{
 
 
@@ -18,7 +18,7 @@ function ServicesTasksId(props){
     })
 
     return (
-    
+
     <div>
 
         <div id="services-task-id">
@@ -27,9 +27,9 @@ function ServicesTasksId(props){
             <ServicesTasksTaskId task={props.taskId[0].tasks}/>
 
             <ServicesFilesTasksId ficheros={props.taskId[0].files}/>
-            
-                <SendMessageWithIcon />
-           
+
+            <SendMessageWithIcon solutioned = {ele[0]?.solutioned} conversation = {ele[0]?.conversation} />
+
         </div>
         <div>
 
