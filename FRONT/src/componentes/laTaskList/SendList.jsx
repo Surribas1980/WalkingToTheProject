eb01a3f7a93abb792c3cf78a40755a8de65c4889
Surribas1980 <issupl@gmail.com>
2022-05-03 07:45:53 +0200

    Without changes

--- FRONT/src/componentes/laTaskList/SendList.jsx
+++ FRONT/src/componentes/laTaskList/SendList.jsx
@@ -2,13 +2,13 @@ import React , { useState } from "react";
 import GenericBoton from "../GenericBoton";
 
 function SendList(props){
-    
+
     const onSend = () =>{
         console.log('Envio datos: ',JSON.stringify(props.list));
     }
 
     return (
-    <>    
+    <>
         <GenericBoton styleDiv='send-data-div' id='send-data' newFunction={onSend} estilo='send-data'/>
     </>
     )
