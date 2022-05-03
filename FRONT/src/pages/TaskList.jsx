070ad50e0f89315f8c44ac8ca783f1842bc4a7b4
Surribas1980 <issupl@gmail.com>
2022-05-03 07:42:49 +0200

    Modified - TaskList.jsx - I've introduced several callback   - FRONT/src/pages/TaskList.jsx

--- FRONT/src/pages/TaskList.jsx
+++ FRONT/src/pages/TaskList.jsx
@@ -8,59 +8,83 @@ import WritenTitleOrList from "../componentes/laTaskList/WritenTitleOrList";
 
 
 function TaskList(){
-    
+
     const [lista,setLista] = useState([]);
     let [titleListTask , setTitleListTask ] = useState("Lista de tareas")
-   
+    let [descriptionTask, setDescriptionTask] = useState("Sin descripcion");
+    let [elFile,setFile]= useState();
+    const valueFile = useRef("");
     const valueInput = useRef("");
     const valueTitle = useRef("");
+    const valueDescription = useRef("");
 
-    
+
     const SaveList = useCallback ((dato)=>{
-        
+
         setLista(()=>{
             const novoObj = new Object();
-            
+
                 novoObj.task = dato;
                 novoObj.done = 'not done';
                 valueInput.current.value= '';
-            
+
             lista.push(novoObj)
-            
+
             return [...lista]
         });
 
         console.log('dentro',lista);
-        
+
     },[lista]);
-    
-    
+
+    const SaveFile = useCallback ((file)=>{
+      setFile(file)
+        let nombreFichero = valueFile.current.files[0].name;
+      console.log('nombreFichero: ',nombreFichero)
+
+      let novoObj = new Object();
+      novoObj.fichero = valueFile.current.files[0].name;
+      lista.push(novoObj);
+/*valueFile.current.file[0].name = '';*/
+console.log('la lista: ',lista)
+
+    },[elFile])
+
     const SaveTitleList = useCallback ((titulo) =>{
         setTitleListTask(titulo)
         valueTitle.current.value= '';
-        
+
         let novoObj = new Object();
         novoObj.title = titulo;
         lista.push(novoObj);
     },[titleListTask]);
+
+    const SaveDescriptionList = useCallback ((descripcion)=>{
+        setDescriptionTask(descripcion);
+        valueDescription.current.value = '';
+        let novoObj = new Object();
+        novoObj.description = descripcion;
+        lista.push(novoObj)
+    })
 
     const DeleteTaskLis = useCallback((index)=>{
         setLista(()=>{
             lista.splice(index,1);
             return [...lista]
         })
-        
-        
+
+
     },[lista]);
 
     return(<>
-   
-    
+
+
     <TitleList title={titleListTask}/>
     <PaintList laFun={DeleteTaskLis} list={lista} />
     <InsertList>
-        <WritenTitleOrList elspan="Tarea" inputref={valueInput}  laFun={SaveList} elplace="Introduce tarea ..."/>
-        <WritenTitleOrList elspan="Cambio de titulo?" inputref={valueTitle} laFun={SaveTitleList} elplace="Introduce titulo de tarea ..."/>
+        <WritenTitleOrList eltype = "text" elspan="Tarea" inputref={valueInput}  laFun={SaveList} elplace="Introduce tarea ..."/>
+        <WritenTitleOrList eltype = "text" elspan="Cambio de titulo?" inputref={valueTitle} laFun={SaveTitleList} elplace="Introduce titulo de tarea ..."/>
+        <WritenTitleOrList eltype = "file" elspan="Introduce fichero" inputref={valueFile} laFun={SaveFile} elplace="Introduce titulo de ficheros"/>
     </InsertList>
 
     <SendList list={lista} />
