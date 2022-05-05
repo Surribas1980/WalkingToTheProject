f5350314da81b77dc3b81c0e92ddef56e197ab77
Surribas1980 <issupl@gmail.com>
2022-05-05 07:47:31 +0200

    Modified - SendMessageWithIcon.jsx - I've modified several conditions to show in case of show it on PC - FRONT/src/componentes/SendMessageWithIcon.jsx

--- FRONT/src/componentes/SendMessageWithIcon.jsx
+++ FRONT/src/componentes/SendMessageWithIcon.jsx
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
 import GenericImageOnClick from './GenericImageOnClick';
 import GenericFormTextArea from './GenericFormTextArea';
 import Conversacion from '../images/conversacion.png';
@@ -7,12 +7,20 @@ import GenericBoton from './GenericBoton';
 import Charla from '../images/charla0.png';
 function SendMessageWithIcon(props){
 const [ver,setVer] = useState(false);
+const [anchura,setAnchura] = useState(false);
 
 /*let formulario = <GenericFormTextArea idDivForm="generic-form-text-area" />;*/
 const changedVer = ()=>{
   let valor = ver;
   setVer(!valor)
 }
+console.log('screen.width',screen.width)
+useEffect(()=>{
+  if(screen.width > 600){
+    setVer(true);
+    setAnchura(true);/**/
+  }
+},[screen.width])
   return (
     <>
     <div className="services-task-id-group">
@@ -21,10 +29,11 @@ const changedVer = ()=>{
         props.solutioned === true ? <GenericBoton styleDiv="services-button"  texto="Ver solución"/> : <GenericImageOnClick idDivImg="generic-image-on-click"  srcImg={Solucionado} functionImg={changedVer} />
       }
 
-         <GenericImageOnClick idDivImg="generic-image-on-click" srcImg={Conversacion} functionImg={changedVer} />
-
       {
-        props.conversation === true ? <GenericImageOnClick idDivImg="generic-image-on-click" srcImg={Charla} functionImg={changedVer} /> : ''
+        anchura === false ?  <GenericImageOnClick idDivImg="generic-image-on-click" srcImg={Conversacion} functionImg={changedVer} /> : ''
+      }
+      {
+        (props.conversation === true) ? <GenericImageOnClick idDivImg="generic-image-on-click" srcImg={Charla} functionImg={changedVer} /> : ''
       }
 
     </div>
