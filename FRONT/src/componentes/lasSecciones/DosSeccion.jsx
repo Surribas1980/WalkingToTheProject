a623bce18d8249a1dac2f8757f0dfdeda30eb5c5
Surribas1980 <issupl@gmail.com>
2022-05-06 13:46:39 +0200

    Modified - DosSeccion.jsx - I've take off the className of the div  - FRONT/src/componentes/lasSecciones/DosSeccion.jsx

--- FRONT/src/componentes/lasSecciones/DosSeccion.jsx
+++ FRONT/src/componentes/lasSecciones/DosSeccion.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function DosSeccion({children}) {
-  return <div className="fondoAzulete">Seccion DOS<div>{children}</div></div>;
+  return <div className="">Seccion DOS<div>{children}</div></div>;
 }
 
 export default DosSeccion;
