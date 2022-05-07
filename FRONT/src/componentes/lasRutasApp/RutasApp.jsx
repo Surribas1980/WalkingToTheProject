4721691e02d08b863e5ce7b0fe465409b28b8b0c
Surribas1980 <issupl@gmail.com>
2022-05-07 11:35:20 +0200

    Modified - RutasApp.jsx - I've commented several routes  - FRONT/src/componentes/lasRutasApp/RutasApp.jsx

--- FRONT/src/componentes/lasRutasApp/RutasApp.jsx
+++ FRONT/src/componentes/lasRutasApp/RutasApp.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import MainApp from '../../pages/MainApp';
-import MyServicesM from '../../pages/MyServices/MyServicesM';
+{/*import MyServicesM from '../../pages/MyServices/MyServicesM';*/}
 import Page_Services from '../../pages/Services/Page-Services';
 import Page_ServicesId from '../../pages/Services/Page-ServicesId';
 import Page_TaskList from '../../pages/Page-TaskList';
-import Solutioned from '../../pages/MyServices/Solutioned';
+{/*import Solutioned from '../../pages/MyServices/Solutioned';*/}
 function RutasApp(){
     return (
     <Routes>
         <Route path={'/mainapp'} element={<MainApp />}/>
-        <Route path={'/myservices'} element={<MyServicesM />}/>
+        {/*<Route path={'/myservices'} element={<MyServicesM />}/>*/}
         <Route path={'/services'} element={<Page_Services />}/>
         <Route path={'/services/:id'} element={<Page_ServicesId />} />
         <Route path={'/tasklist'} element={<Page_TaskList />} />
-        <Route path={'/myservicessolutioned'} element={<Solutioned />} />
+        {/*<Route path={'/myservicessolutioned'} element={<Solutioned />} />*/}
     </Routes>
     )
 }
