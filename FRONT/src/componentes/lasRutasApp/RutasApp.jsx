641a7bd99c5e09c138df731b57fcf8250b50b3ee
Surribas1980 <issupl@gmail.com>
2022-05-10 13:26:51 +0200

    Modified - RutasApp.jsx - I've introduced a component that is send with route's component  - FRONT/src/componentes/lasRutasApp/RutasApp.jsx

--- FRONT/src/componentes/lasRutasApp/RutasApp.jsx
+++ FRONT/src/componentes/lasRutasApp/RutasApp.jsx
@@ -6,6 +6,7 @@ import Page_Services from '../../pages/Services/Page-Services';
 import Page_ServicesId from '../../pages/Services/Page-ServicesId';
 import Page_TaskList from '../../pages/Page-TaskList';
 import Page_Personal_Conversation_Id from '../../pages/Services/Page-Personal.Conversation';
+import GenericFormTextArea from '../GenericFormTextArea';
 {/*import Solutioned from '../../pages/MyServices/Solutioned';*/}
 function RutasApp(){
     return (
@@ -14,7 +15,7 @@ function RutasApp(){
         {/*<Route path={'/myservices'} element={<MyServicesM />}/>*/}
         <Route path={'/services'} element={<Page_Services />}/>
         <Route path={'/services/:id'} element={<Page_ServicesId />}>
-            <Route path={'conversation/:idUsuario'} element={<Page_Personal_Conversation_Id />} />
+            <Route path={'conversation/:idUsuario'} element={<Page_Personal_Conversation_Id dato={<GenericFormTextArea idDivForm="generic-form-text-area" />} />} />
         </Route>
         {/*<Route path={'/services/:id'} element={<Page_ServicesId />} />
         <Route path={'/services/:id/conversation/:idUsuario'} element={<Page_Personal_Conversation_Id />} />*/}
