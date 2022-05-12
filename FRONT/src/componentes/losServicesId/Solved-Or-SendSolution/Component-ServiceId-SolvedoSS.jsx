eaede3eedc38ca45b4df4519956bc175dce0a487
Surribas1980 <issupl@gmail.com>
2022-05-12 07:48:19 +0200

    Modified - Component-ServiceId-SolvedoSS.jsx - I've introduced to show the files  -  FRONT/src/componentes/losServicesId/Solved-Or-SendSolution/Component-ServiceId-SolvedoSS.jsx

--- FRONT/src/componentes/losServicesId/Solved-Or-SendSolution/Component-ServiceId-SolvedoSS.jsx
+++ FRONT/src/componentes/losServicesId/Solved-Or-SendSolution/Component-ServiceId-SolvedoSS.jsx
@@ -1,10 +1,46 @@
-import React from "react";
+import React,{useState} from "react";
+
+
+import Docx from '../../../images/documents/archivo-word.png';
+import Xlsx from '../../../images/documents/archivo-xlsx.png';
+import Pptx from '../../../images/documents/archivo-powerpoint.png';
+
 
 function Component_ServiceId_SolvedoSS(props){
+  const [archivos,setArchivo] = useState([Object.values(props.ficheros)])
+  let salida = archivos[0].map((item,index)=>{
+    let thePath;
+    const urlstatic = "/FRONT/src/documentos/";
 
+    if(item.slice(-4) == "docx"){
+      thePath = Docx;
+
+    }
+    if(item.slice(-4) == "xlsx"){
+      thePath = Xlsx;
+    }
+    if(item.slice(-4) == "pptx"){
+      thePath = Pptx;
+    }
+    let dir = urlstatic + `${item}`;
+    let enlazar = dir.slice(14);
+    return (<div className="son">
+      <img id="icono" src={thePath}/>
+      <a href={dir}>
+        <span name="task">{item}</span>
+      </a>
+
+    </div>)
+
+  })
+console.log('la salida: ', props.ficheros)
     return (<>
-        <div>
-        Component_ServiceId_SolvedoSS
+
+      Resuelto<div>Puntuación : </div>
+        <div id='solved'>
+
+        {salida}
+
         </div>
         </>)
 }
