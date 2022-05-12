0a123b80e7961e70dd1ab8422d99698676ac8b60
Surribas1980 <issupl@gmail.com>
2022-05-12 07:45:55 +0200

    Modified - Component-ServiceId-SoSendSolution.jsx - I've modified several divs and I've included the input and button  - FRONT/src/componentes/losServicesId/Solved-Or-SendSolution/Component-ServiceId-SoSendSolution.jsx

--- FRONT/src/componentes/losServicesId/Solved-Or-SendSolution/Component-ServiceId-SoSendSolution.jsx
+++ FRONT/src/componentes/losServicesId/Solved-Or-SendSolution/Component-ServiceId-SoSendSolution.jsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React,{useRef} from "react";
 
 function Component_ServiceId_SoSendSolution(props){
+  const theInput = useRef(null);
 
+  const SendSolution = (data)=>{
+    console.log('SendSolution: ', data,theInput.current.files,theInput.current.files[0])
+  }
 
     return(<>
     <div>
-    Component_ServiceId_SoSendSolution
+
+    <div>Enviar Solución</div>
+      <input name="solution" ref={theInput} multiple type="file" placeholder="otro"/>
+      <button name="solution" onClick={()=>{SendSolution(theInput.current.value)}}>Enviar</button>
+
     </div>
     </>)
 
