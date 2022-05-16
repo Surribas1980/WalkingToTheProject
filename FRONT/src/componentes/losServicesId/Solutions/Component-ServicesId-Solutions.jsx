2052aa2f909338188c6095285bb642dff99179a6
Surribas1980 <issupl@gmail.com>
2022-05-16 07:55:53 +0200

    Modified - Component-ServicesId-Solutions.jsx - I've changed several lines to try to pass props to the other component  - FRONT/src/componentes/losServicesId/Solutions/Component-ServicesId-Solutions.jsx

--- FRONT/src/componentes/losServicesId/Solutions/Component-ServicesId-Solutions.jsx
+++ FRONT/src/componentes/losServicesId/Solutions/Component-ServicesId-Solutions.jsx
@@ -1,8 +1,13 @@
-import React from "react";
+import React,{useState} from "react";
+import Component_ServicesId_To_Confirm_SolutionId from './Component-ServicesId-To-Confirm-SolutionId';
+
+import {myPossiblePersonSolved} from '../../../data.js';
 
 function Component_ServicesId_SolutionsId(props){
+const [possibleSolved,setPossibleSolved] = useState(myPossiblePersonSolved())
 
-    return(<></>)
+console.log('Component_ServicesId_SolutionsId: ',possibleSolved)
+    return(<><Component_ServicesId_To_Confirm_SolutionId solucionadores={possibleSolved}/></>)
 }
 
 export default Component_ServicesId_SolutionsId;
