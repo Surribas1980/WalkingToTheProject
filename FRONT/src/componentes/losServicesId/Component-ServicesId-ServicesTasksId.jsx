94da221046e74a8c33aabfd7a6273eae073065d4
Surribas1980 <issupl@gmail.com>
2022-05-16 13:40:34 +0200

    Modified - Component-ServicesId-ServicesTasksId.jsx - I've introduced the component that will view with props  - FRONT/src/componentes/losServicesId/Component-ServicesId-ServicesTasksId.jsx

--- FRONT/src/componentes/losServicesId/Component-ServicesId-ServicesTasksId.jsx
+++ FRONT/src/componentes/losServicesId/Component-ServicesId-ServicesTasksId.jsx
@@ -5,24 +5,14 @@ import SendMessageWithIcon from "../SendMessageWithIcon";
 import Component_ServicesId_Conversations from "./Conversations/Component-ServicesId-Conversations";
 import GenericFormTextArea from "../GenericFormTextArea";
 import Component_ServicesId_Conversations_ColectiveConversationsId from "./Conversations/ColectiveConversationsId/Component-ServicesId-Conversations-ColectiveConversationsId";
-import Page_Services from "../../pages/Services/Page-Services";
 import Auxiliary_Page_ServiceId_Conversations from '../../pages/Services/ServicesId-Conversations/Auxiliary_Page_ServiceId_Conversations-Page-Conversations-Of-MyServiceId';
 import Auxiliary_Page_ServiceId_Solved_Or_SendSolution from "../../pages/Services/ServicesId-Solved-Or-SendSolution/Auxiliary-Page-ServiceId-Solved-Or-SendSolution";
 import Component_ServicesId_SolutionsId from './Solutions/Component-ServicesId-Solutions.jsx';
 function Component_ServicesId_ServicesTasksTaskId(props){
-  const [visualizo,setVisu] = useState(false);
+  
     let ele = props.taskId;
     console.log(ele[0]?.id,ele[0]?.solved,ele,'props: ',props,'props.data: ',props.data)
-    let misServe;
-    if(props.data === "misserviciosid"){
-
-       setVisu(true)
-
-
-       console.log('estoy en donde quiero: ', misServe)
-    }else{
-      misServe = '';
-    }
+   
 
     let salida = props.taskId?.map((item,index)=>{
 
@@ -44,13 +34,13 @@ function Component_ServicesId_ServicesTasksTaskId(props){
         <div id="services-task-id">
             {salida}
             <div name="titulo-lista-tareas">Tareas</div>
-             <Component_ServicesId_SolutionsId />
+            
             <Component_ServicesId_ServicesTasksId_ServicesTasksTaskId task={props.taskId[0].tasks}/>
 
             <Component_ServicesId_ServicesTasksId_ServicesFilesTasksId ficheros={props.taskId[0].files}/>
 
             <SendMessageWithIcon solved = {ele[0]?.solved} conversation = {ele[0]?.conversation} />
-            {visualizo === true ? <Component_ServicesId_SolutionsId /> : ''}
+            {props.visualizo === true ? props.data : ''}
         </div>
         <div className="services-task-id-solution-conversation">
             <div className="solution">
