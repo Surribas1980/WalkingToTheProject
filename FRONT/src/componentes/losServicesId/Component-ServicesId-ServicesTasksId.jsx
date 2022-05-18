c1d53dd45009b258e067c1682a1055fd72b8b015
Surribas1980 <issupl@gmail.com>
2022-05-18 22:21:39 +0200

    Modified - Component-ServicesId-ServicesTasksId.jsx - I've introduced the conversationsOrder by filter and a component 'Component-ServicesId-Conversations' - FRONT/src/componentes/losServicesId/Component-ServicesId-ServicesTasksId.jsx

--- FRONT/src/componentes/losServicesId/Component-ServicesId-ServicesTasksId.jsx
+++ FRONT/src/componentes/losServicesId/Component-ServicesId-ServicesTasksId.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState} from "react";
 import Component_ServicesId_ServicesTasksId_ServicesTasksTaskId from "./Component-ServicesId-ServicesTasksTaskId";
 import Component_ServicesId_ServicesTasksId_ServicesFilesTasksId from "./Component-ServicesId-ServicesFilesTasksId";
 
@@ -8,19 +8,29 @@ import Component_ServicesId_Conversations_ColectiveConversationsId from "./Conve
 import Auxiliary_Page_ServiceId_Conversations from '../../pages/Services/ServicesId-Conversations/Auxiliary_Page_ServiceId_Conversations-Page-Conversations-Of-MyServiceId';
 import Auxiliary_Page_ServiceId_Solved_Or_SendSolution from "../../pages/Services/ServicesId-Solved-Or-SendSolution/Auxiliary-Page-ServiceId-Solved-Or-SendSolution";
 
+import Component_ServicesId_ConversationsId from './Conversations/Component-ServicesId-ConversationsId';
+
+import {getPersonalConversationsId} from '../../data.js';
+
 function Component_ServicesId_ServicesTasksTaskId(props){
-  
-    let ele = props.taskId;
-    console.log(ele[0]?.id,ele[0]?.solved,ele,'props: ',props,'props.data: ',props.data)
-   
-    let enviarSolution = 
+    const [conversationId,setConversationId] = useState(getPersonalConversationsId());
+    let ele = props.taskId
+
+    const lasConversationsIdOrdenadas = conversationId?.filter((item)=>{
+      return ele[0]?.id == item.id;
+    }).sort((idbbddn0,idbbddn1)=>{
+      return idbbddn0.idbbdd > idbbddn1.idbbdd });
+    console.log('lasConversationsIdOrdenadas: ',lasConversationsIdOrdenadas,'.. ele[0]?.id: ',ele[0]?.id,ele[0]?.solved,ele,'props: ',props,'props.data: ',props.data)
+
+    let enviarSolution =
             <div className="solution">
                 <Auxiliary_Page_ServiceId_Solved_Or_SendSolution solved = {ele[0]?.solved} solution = {ele[0]?.solution} />
-            </div> 
+            </div>
      ;
     let personalConversations = <div className="solution">
     <Component_ServicesId_Conversations >
-        <Auxiliary_Page_ServiceId_Conversations elId={ele[0]?.id} />
+        {/*<Auxiliary_Page_ServiceId_Conversations elId={ele[0]?.id} />*/}
+        <Component_ServicesId_ConversationsId ConversationsId={lasConversationsIdOrdenadas} />
     </Component_ServicesId_Conversations>
 </div>;
 
@@ -44,16 +54,16 @@ function Component_ServicesId_ServicesTasksTaskId(props){
         <div id="services-task-id">
             {salida}
             <div name="titulo-lista-tareas">Tareas</div>
-            
+
             <Component_ServicesId_ServicesTasksId_ServicesTasksTaskId task={props.taskId[0].tasks}/>
 
             <Component_ServicesId_ServicesTasksId_ServicesFilesTasksId ficheros={props.taskId[0].files}/>
 
-           
+
             {props.visualizo === true ? props.data : ''}
         </div>
         <div className="services-task-id-solution-conversation">
-            
+
             {props.visualizo === true ? '' : enviarSolution}
             {personalConversations}
             <div className="conversation-total">
