6575ffc2af1d3f4b75ecd73c4bc209a545a10060
Surribas1980 <issupl@gmail.com>
2022-05-18 13:47:07 +0200

    Modified - Component-ServicesId-ServicesTasksId.jsx - I was working to show better the conversation  - FRONT/src/componentes/losServicesId/Component-ServicesId-ServicesTasksId.jsx

--- FRONT/src/componentes/losServicesId/Component-ServicesId-ServicesTasksId.jsx
+++ FRONT/src/componentes/losServicesId/Component-ServicesId-ServicesTasksId.jsx
@@ -1,18 +1,28 @@
-import React,{useState,useEffect} from "react";
+import React from "react";
 import Component_ServicesId_ServicesTasksId_ServicesTasksTaskId from "./Component-ServicesId-ServicesTasksTaskId";
 import Component_ServicesId_ServicesTasksId_ServicesFilesTasksId from "./Component-ServicesId-ServicesFilesTasksId";
-import SendMessageWithIcon from "../SendMessageWithIcon";
+
 import Component_ServicesId_Conversations from "./Conversations/Component-ServicesId-Conversations";
 import GenericFormTextArea from "../GenericFormTextArea";
 import Component_ServicesId_Conversations_ColectiveConversationsId from "./Conversations/ColectiveConversationsId/Component-ServicesId-Conversations-ColectiveConversationsId";
 import Auxiliary_Page_ServiceId_Conversations from '../../pages/Services/ServicesId-Conversations/Auxiliary_Page_ServiceId_Conversations-Page-Conversations-Of-MyServiceId';
 import Auxiliary_Page_ServiceId_Solved_Or_SendSolution from "../../pages/Services/ServicesId-Solved-Or-SendSolution/Auxiliary-Page-ServiceId-Solved-Or-SendSolution";
-import Component_ServicesId_SolutionsId from './Solutions/Component-ServicesId-Solutions.jsx';
+
 function Component_ServicesId_ServicesTasksTaskId(props){
   
     let ele = props.taskId;
     console.log(ele[0]?.id,ele[0]?.solved,ele,'props: ',props,'props.data: ',props.data)
    
+    let enviarSolution = 
+            <div className="solution">
+                <Auxiliary_Page_ServiceId_Solved_Or_SendSolution solved = {ele[0]?.solved} solution = {ele[0]?.solution} />
+            </div> 
+     ;
+    let personalConversations = <div className="solution">
+    <Component_ServicesId_Conversations >
+        <Auxiliary_Page_ServiceId_Conversations elId={ele[0]?.id} />
+    </Component_ServicesId_Conversations>
+</div>;
 
     let salida = props.taskId?.map((item,index)=>{
 
@@ -39,27 +49,28 @@ function Component_ServicesId_ServicesTasksTaskId(props){
 
             <Component_ServicesId_ServicesTasksId_ServicesFilesTasksId ficheros={props.taskId[0].files}/>
 
-            <SendMessageWithIcon solved = {ele[0]?.solved} conversation = {ele[0]?.conversation} />
+           
             {props.visualizo === true ? props.data : ''}
         </div>
         <div className="services-task-id-solution-conversation">
-            <div className="solution">
-                <Auxiliary_Page_ServiceId_Solved_Or_SendSolution solved = {ele[0]?.solved} solution = {ele[0]?.solution} />
-            </div>
-            <div className="solution">
-                <Component_ServicesId_Conversations >
-                    <Auxiliary_Page_ServiceId_Conversations elId={ele[0]?.id} />
-                </Component_ServicesId_Conversations>
-            </div>
+            
+            {props.visualizo === true ? '' : enviarSolution}
+            {personalConversations}
             <div className="conversation-total">
 
                 <Component_ServicesId_Conversations dato={<GenericFormTextArea idDivForm="generic-form-text-area"/>}>
-                    <Component_ServicesId_Conversations_ColectiveConversationsId />
-                    <Component_ServicesId_Conversations_ColectiveConversationsId />
-                    <Component_ServicesId_Conversations_ColectiveConversationsId />
-                    <Component_ServicesId_Conversations_ColectiveConversationsId />
-                    <Component_ServicesId_Conversations_ColectiveConversationsId />
-                    <Component_ServicesId_Conversations_ColectiveConversationsId />
+                    <div className="conversation-colective">
+                        <Component_ServicesId_Conversations_ColectiveConversationsId />
+                    </div>
+                    <div className="conversation-colective">
+                        <Component_ServicesId_Conversations_ColectiveConversationsId />
+                    </div>
+                    <div className="conversation-colective">
+                        <Component_ServicesId_Conversations_ColectiveConversationsId />
+                    </div>
+                    <div className="conversation-colective">
+                        <Component_ServicesId_Conversations_ColectiveConversationsId />
+                    </div>
 
                 </Component_ServicesId_Conversations>
             </div>
