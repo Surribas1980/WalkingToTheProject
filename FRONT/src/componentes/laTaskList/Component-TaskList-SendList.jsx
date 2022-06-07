18e6269b5397f75e917218d6e4ab252ab34369ca
Surribas1980 <issupl@gmail.com>
2022-06-07 13:26:59 +0200

    Modified - I've introduced useEffect to save the files  - src/componentes/laTaskList/Component-TaskList-SendList.jsx

--- FRONT/src/componentes/laTaskList/Component-TaskList-SendList.jsx
+++ FRONT/src/componentes/laTaskList/Component-TaskList-SendList.jsx
@@ -1,10 +1,28 @@
 import React , { useState } from "react";
 import GenericBoton from "../GenericBoton";
 
+/*--- Functions on http ---*/
+import { endpoints } from '../../http/endpoints';
+import { envioWithFiles } from '../../http/api';
+import { method } from '../../http/method';
+
+
+/*--- misHelpers ---*/
+import { theFormData } from '../../helpers/misHelpers';
+import { envioDatos } from '../../http/api';
+
 function Component_TaskList_SendList(props){
 
-    const onSend = () =>{
-        console.log('Envio datos: ',JSON.stringify(props.list));
+    const onSend = async() =>{
+
+        const datos = {
+          form: theFormData(props,1),
+          endpoint: endpoints.sendService,
+          method: method.post
+        }
+        const valorrespuesta = await envioDatos(datos,'form')
+
+        console.log('valorrespuesta : ',valorrespuesta)
     }
 
     return (
