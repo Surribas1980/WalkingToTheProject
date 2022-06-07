7ae0d411d1f7fa2f6ee33b01376371caa70a85bc
Surribas1980 <issupl@gmail.com>
2022-06-07 13:29:57 +0200

    Modified - I've introduced the function endpoints  - src/componentes/laTaskList/Component-TaskList-WritenTitleOrList.jsx

--- FRONT/src/componentes/laTaskList/Component-TaskList-WritenTitleOrList.jsx
+++ FRONT/src/componentes/laTaskList/Component-TaskList-WritenTitleOrList.jsx
@@ -1,7 +1,13 @@
-import React, { memo } from "react";
+import React, { useEffect,useState,memo } from "react";
 
 function Component_TaskList_WritenTitleOrList (props){
-    
+const [theFiles,setTheFiles] = useState(null);
+  useEffect(()=>{
+    console.log('Desde WriteTitleOrList ', props.inputref.current,props.inputref.current.files,props.inputref.current.value)
+    setTheFiles(props.inputref.current.files);
+    console.log('los ficheros: ',theFiles)
+  },[props.inputref.current.value]);
+
     let salida;
     if(props.eltype == "text"){
         salida = <><input type={props.eltype} ref={props.inputref}  placeholder={props.elplace}/></>;
@@ -11,10 +17,12 @@ function Component_TaskList_WritenTitleOrList (props){
     }else if(props.eltype == "textarea"){
         salida =<><textarea type={props.eltype} rows="10" cols="50"  ref={props.inputref}  placeholder={props.elplace}/></>;
     }
+
+
 return(
 <div className="" id="writinglist">
     <span>{props.elspan}</span>
-    
+
     {salida}
     <button className="save-data-default" onClick={()=>{props.laFun(props.inputref.current.value)}}>Guardar</button>
 </div>)
