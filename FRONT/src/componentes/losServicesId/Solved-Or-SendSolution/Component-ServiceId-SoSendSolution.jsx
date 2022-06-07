9aebd154cdbbbaee1d93428be6648ed8c1cd3517
Surribas1980 <issupl@gmail.com>
2022-06-07 13:43:16 +0200

    Modified - I've introduced one function async and the data that contain the data that is needed to send  - src/componentes/losServicesId/Solved-Or-SendSolution/Component-ServiceId-SoSendSolution.jsx

--- FRONT/src/componentes/losServicesId/Solved-Or-SendSolution/Component-ServiceId-SoSendSolution.jsx
+++ FRONT/src/componentes/losServicesId/Solved-Or-SendSolution/Component-ServiceId-SoSendSolution.jsx
@@ -1,10 +1,30 @@
 import React,{useRef} from "react";
+import { envioDatos } from "../../../http/api";
+import { theFormData } from '../../../helpers/misHelpers';
+
+/*--- misHelpers ---*/
+import { method } from '../../../http/method';
+import { endpoints } from '../../../http/endpoints';
 
 function Component_ServiceId_SoSendSolution(props){
   const theInput = useRef(null);
 
-  const SendSolution = (data)=>{
-    console.log('SendSolution: ', data,theInput.current.files,theInput.current.files[0])
+  const SendSolution = async (data)=>{
+    console.log('SendSolution: ', theInput.current.files)
+    const losFich = {
+      ficheros: theInput.current.files
+    }
+    const datos = {
+      form: theFormData(losFich,0),
+      endpoint: endpoints.sendServiceSolution,
+      method: method.post,
+      id:props.id
+    }
+
+    console.log('los ficheros: ',losFich)
+    const valorrespuesta = await envioDatos(datos,'form');
+    console.log('valorrespuesta : ',valorrespuesta)
+
   }
 
     return(<>
