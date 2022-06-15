1bb17899ae4ab9edf194e873c0af4a0d95570c48
Surribas1980 <issupl@gmail.com>
2022-06-15 23:57:22 +0200

    Probe with Firebase Store

--- FRONT/src/pages/Page-TaskList.jsx
+++ FRONT/src/pages/Page-TaskList.jsx
@@ -1,5 +1,31 @@
 import React, {useState,useRef, useCallback} from "react";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getStorage, ref, uploadBytes,uploadString } from "firebase/storage"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: ,
+  authDomain: ,
+  databaseURL: ,
+  projectId: ,
+  storageBucket: ,
+  messagingSenderId: ,
+  appId: ,
+  measurementId: 
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+/**
+ * 
+ */
 import Component_TaskList_InsertList from "../componentes/laTaskList/Component-TaskList-InsertList";
 import Component_TaskList_PaintList from "../componentes/laTaskList/Component-TaskList-PaintList";
 import Component_TaskList_SendList from "../componentes/laTaskList/Component-TaskList-SendList";
@@ -20,7 +46,9 @@ function Page_TaskList(){
     const valueInput = useRef("");
     const valueTitle = useRef("");
     const valueDescription = useRef("");
-
+    const storage = getStorage();
+    const storageRef = ref(storage, 'some');
+    
 
     const SaveList = useCallback ((dato)=>{
 
@@ -44,7 +72,9 @@ function Page_TaskList(){
 
         let valor = valueFile.current.files;
 
-
+        uploadBytes(storageRef, valueFile.current.files[0]).then((snapshot) => {
+            console.log('Uploaded a blob or file!');
+          });
         for (let i = 0 ; i < valor.length ; i++){
 
             setFile(valueFile.current.files[i].name);
@@ -70,6 +100,21 @@ function Page_TaskList(){
         let novoObj = new Object();
         novoObj.title = titulo;
         lista.push(novoObj);
+                const bytes = new Uint8Array([0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x2c, 0x20, 0x77, 0x6f, 0x72, 0x6c, 0x64, 0x21]);
+                uploadBytes(storageRef, bytes).then((snapshot) => {
+                console.log('Uploaded an array!');
+                });
+            // Base64 formatted string
+            const message2 = 'Q2FkZW5hIGEgY29kaWZpY2Fy';
+            uploadString(storageRef, message2, 'base64').then((snapshot) => {
+            console.log('Uploaded a base64 string!');
+            });
+            
+            // Base64url formatted string
+            /*const message3 = '5b6p5Y-344GX44G-44GX44Gf77yB44GK44KB44Gn44Go44GG77yB';
+            uploadString(storageRef, message3, 'base64url').then((snapshot) => {
+            console.log('Uploaded a base64url string!');
+            });*/
     },[titleListTask]);
 
     const SaveDescriptionList = useCallback ((descripcion)=>{
